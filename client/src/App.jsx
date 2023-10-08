ca98c572e7da6433fe8c530390c26bfa54624011
Willie <kemawilly@gmail.com>
2023-10-08 23:53:06 +0100

    Pages Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
 export default function App() {
-  return <h1 className="text-red-500">App</h1>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign_in" element={<SignIn />} />
+        <Route path="/sign_out" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
